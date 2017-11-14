0898f3c4aa0a67ac38a2bdb3687eacef01a3f139
Billy Rennekamp <billy.rennekamp@gmail.com>
2017-11-14 18:18:54 +0100

    ready for attempt

--- bin/dapp-scratch.js
+++ bin/dapp-scratch.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 var DappScratch = require("../lib/dapp-scratch")
 
-var commander = require('commander');
-var colors = require('colors');
+var commander = require('commander')
+var colors = require('colors')
 var fs = require('fs')
+var shell = require('shelljs')
+var inquirer = require('inquirer')
+const child_process = require('child_process');
 
 commander
     .version('0.0.1')
+    .option('-a, --address', 'Address')
+    .option('-b, --abi', 'ABI')
     .option('-c, --contract', 'Contract')
-    .option('-a, --abi', 'ABI')
 
 
 commander
@@ -55,6 +59,56 @@ function getABI (path) {
     }
     return path
 }
+
+function installDependencies () {
+    return new Promise((resolve, reject) => {
+        let dependencies = ['bignumber.js', 'web3', 'web3-provider-engine', '@aeternity/id-manager-provider']
+        let depIndex = 0
+        let package = shell.exec('ls package.json', {silent: true})
+        if (package.stderr) {
+            inquirer.prompt({
+                type: 'confirm',
+                name: 'npminit',
+                message: 'package.json not detected. OK to init?'
+            }).then((resp) => {
+                if (resp.npminit) {
+                    child_process.execFileSync('npm', ['init'], {stdio: 'inherit'})
+                    promptInstall(0, dependencies).then(resolve).catch(reject)
+                } else {
+                    console.log(colors.yellow('Dependencies will not be installed'))
+                    resolve()
+                }
+            }).catch(reject)
+        } else{
+            promptInstall(0, dependencies).then(resolve).catch(reject)
+        }
+    })
+}
+
+function promptInstall (index = 0, dependencies = []) {
+    return new Promise((resolve, reject) => {
+        if (index >= dependencies.length) resolve()
+        let dep = dependencies[index]
+        if (shell.exec('npm list |  grep ' + dep, {silent:true}).stdout.length) {
+            console.log(colors.green(dep + ' detected'))
+            promptInstall(index + 1, dependencies).then(resolve).catch(reject)
+        }else {
+            inquirer.prompt({
+                type: 'confirm',
+                name: 'install_' + dep.split('.').join('_'),
+                message: 'Ok to install and add ' + dep + ' to your package.json?'
+            }).then((resp) => {
+                if (resp['install_' + dep.split('.').join('_')]) {
+                    child_process.execFileSync('npm', ['install', dep, '--save'], {stdio: 'inherit'})
+                    console.log(colors.green(dep + ' installed'))
+                } else {
+                    console.log(colors.yellow(dep + ' will not be installed'))
+                }
+                promptInstall(index + 1, dependencies).then(resolve).catch(reject)
+            }).catch(reject)
+        }
+    })
+}
 commander
     .command('* <filename>')
     .description('Build Wrapper from Contract')
@@ -69,12 +123,29 @@ commander
             console.log(colors.green('ABI found at ' + abiPath));
             dappScratch = new DappScratch({abiPath})
         }
-        dappScratch.createWrapper()
+        installDependencies().then(() => {
+            return dappScratch.createWrapper(commander.address)
+        })
         .then(() => {
-          console.log('Wrapper Created 🍾'.green)
+          console.log(colors.green('Wrapper Created at ' + dappScratch.path))
+          console.log(colors.grey(`
+/*
+ * To use ${dappScratch.projectName} just import it into your project:
+ */
+
+import ${dappScratch.projectName} from '${dappScratch.path}'
+let ${dappScratch.projectNameSmall} = new ${dappScratch.projectName}()
+${dappScratch.projectNameSmall}.helloWorld()
+
+/*
+ * Have Fun : )
+ */
+            `))
+          process.exit(1);
         })
         .catch((error) => {
             console.error(colors.red(error))
+            commander.help()
         })
     })
 // process.on('unhandledRejection', (reason) => {
